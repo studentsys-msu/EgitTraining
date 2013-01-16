3ee4d9b1c771fd0d8bca809d5b862d22793d13bd
Elizabeth Henderson <ehender@msu.edu>
2013-01-16 14:36:42 -0500

    Added comments

--- src/edu/msu/egit/training/ConflictTesting.java
+++ src/edu/msu/egit/training/ConflictTesting.java
@@ -3,6 +3,8 @@ package edu.msu.egit.training;
 public class ConflictTesting {
 	
 	public static void main(String[] args) {
+		
+		//Some comments here
 		System.out.println("Hello world.");
 		
 		
