e52a60fe420d3eb2862e6ae79b65dd096ff54dd8
Rich Czarnecki <czarne31@msu.edu>
2013-01-16 14:35:26 -0500

    Rich changed.

--- src/edu/msu/egit/training/ConflictTesting.java
+++ src/edu/msu/egit/training/ConflictTesting.java
@@ -5,7 +5,7 @@ public class ConflictTesting {
 	public static void main(String[] args) {
 		System.out.println("Hello world.");
 		
-		
+		// Rich added.
 		System.out.println("Rich says hello.");
 	}
 
