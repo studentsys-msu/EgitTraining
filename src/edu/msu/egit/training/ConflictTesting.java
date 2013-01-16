dffccc716193252fcb11bf376628d73bb663d784
Elizabeth Henderson <ehender@msu.edu>
2013-01-16 14:27:44 -0500

    Add space

--- src/edu/msu/egit/training/ConflictTesting.java
+++ src/edu/msu/egit/training/ConflictTesting.java
@@ -3,7 +3,7 @@ package edu.msu.egit.training;
 public class ConflictTesting {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello world.");
+		System.out.println("Hello world. ");
 	}
 
 }
