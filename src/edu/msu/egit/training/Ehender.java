71efcb09b3bc90930d893a1dd9cf917068e410b5
Elizabeth Henderson <ehender@msu.edu>
2013-01-18 10:27:59 -0500

    Added some more code.

--- src/edu/msu/egit/training/Ehender.java
+++ src/edu/msu/egit/training/Ehender.java
@@ -3,6 +3,8 @@ package edu.msu.egit.training;
 public class Ehender {
 	public static void main(String[] args) {
 		System.out.println("Hello World.");
+		
+		System.out.println("Egit Training");
 	}
 
 }
